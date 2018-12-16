41e1fcaf6ac83f4b90f5510bafb265abeca02269
Pablo Rojas <projascifre@gmail.com>
2018-12-16 21:38:13 +0100

    test /hola

--- src/main/testserver.go
+++ src/main/testserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"strings"
 )
@@ -11,8 +12,23 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	message = "Estoy funcionando!!!!-------------------" + message
 	w.Write([]byte(message))
 }
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "rds_pharmacy_00:phar00macy@tcp(rdspharmacy00.ctiytnyzqbi7.us-east-2.rds.amazonaws.com)/rdspharmacy00")
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Get Userv !!!-db: " + db + " error: " + err
+	w.Write([]byte(message))
+	sayUser(d)
+}
+
 func main() {
 	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/hola", getUser)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
