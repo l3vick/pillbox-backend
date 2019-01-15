8583cda1b22ddcf67dea8d9392fa612851bc6828
Pablo Rojas <projascifre@gmail.com>
2019-01-15 23:26:21 +0100

    nullsql utils and meds request updated

--- model/med.go
+++ model/med.go
@@ -1,8 +1,19 @@
 package model
 
-//Med ...
+import (
+	"github.com/l3vick/go-pharmacy/util"
+)
+
 type Med struct {
 	ID   int	`json:"id"`
 	Name string `json:"name"`
-	Pvp  int    `json:"pvp"`
+	Description  string    `json:"description"`
+	PharmacyID  util.JsonNullInt64 `json:"pharmacy_id,omitempty"`
+}
+
+type MedInt struct {
+	ID   int	`json:"id"`
+	Name string `json:"name"`
+	Description  string    `json:"description"`
+	PharmacyID  int `json:"pharmacy_id,omitempty"`
 }
--- model/user.go
+++ model/user.go
@@ -1,6 +1,5 @@
 package model
 
-//User ...
 type User struct {
 	ID              int		`json:"id"`
 	Name            string	`json:"name"`
