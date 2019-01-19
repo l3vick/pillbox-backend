257a9032732a7dc1c2ed1cc6847bd82fdcf109e9
charlybiersack <carlos_lallama@hotmail.com>
2019-01-19 01:14:21 +0100

    aaa

--- model/treatment.go
+++ model/treatment.go
@@ -1,11 +1,11 @@
 package model
 
 type Treatment struct {
-	ID              int			`json:"id"`
-	IDMed           int			`json:"id_med"`
-	IDUser   		int			`json:"id_user"`
-	Morning     	bool		`json:"morning"`
-	Afternoon      	bool		`json:"afternoon"`
-	Evening 		bool		`json:"evening"`
-	EndTreatment    string		`json:"end_treatment"`
+	ID           int    `json:"id"`
+	IDMed        int    `json:"id_med"`
+	IDUser       int    `json:"id_user"`
+	Morning      bool   `json:"morning"`
+	Afternoon    bool   `json:"afternoon"`
+	Evening      bool   `json:"evening"`
+	EndTreatment string `json:"end_treatment"`
 }
